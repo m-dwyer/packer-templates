dcd9ebbba2565e5182e56581dd76c70f8384d90e
M Dwyer <mark.andrew.dwyer@gmail.com>
2015-08-10 22:14:11 +1000

    Fix call to sc.exe

--- installconfig/windows/enablewinrm.ps1
+++ installconfig/windows/enablewinrm.ps1
@@ -4,4 +4,6 @@ winrm quickconfig -q -transport:http
 winrm set winrm/config/client '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
-sc config winrm start=auto
+net stop winrm
+sc.exe config winrm start=auto
+net start winrm
