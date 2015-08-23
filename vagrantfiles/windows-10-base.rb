1a3378f631f63c2547ea9e713a29484aad43f5b2
M Dwyer <mark.andrew.dwyer@gmail.com>
2015-08-23 12:09:10 +1000

    Provision vagrant box to map vagrant share to V:\

--- vagrantfiles/windows-10-base.rb
+++ vagrantfiles/windows-10-base.rb
@@ -5,4 +5,8 @@ Vagrant.configure(2) do |config|
 	config.vm.communicator = :winrm
 	config.vm.network :forwarded_port, guest: 3389, host: 3389, id: "rdp"
 	config.vm.network :forwarded_port, guest: 5985, host: 5985, id: "winrm"
+
+	config.vm.provision "shell",
+	  inline: "New-PSDrive -Name \"V\" -PSProvider FileSystem -Root \"\\\\VBOXSVR\\vagrant\" -Persist"
+
 end
